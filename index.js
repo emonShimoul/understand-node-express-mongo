7d8f46dbc4b2e6b9df05bc274c8954187406fb10
emonShimoul <emonshimoul@gmail.com>
2022-07-03 12:15:59 +0600

    add try and finally inside the run function

--- index.js
+++ index.js
@@ -8,12 +8,22 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try{
+
+    }
+    finally{
+        // await client.close()
+    }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
 
 /*
@@ -31,7 +41,10 @@ Every Projects
 5. create or get database access credentials (username, password)
 6. create .env file and add DB_USER and DB_PASS as environment variable
 7. make sure you require (import) dotenv
-8. Convert URI string to a template string. 
-9. Add DB_USER and DB_PASS in the connection URI string
-10. Check URI string by using console.log()
+8. convert URI string to a template string. 
+9. add DB_USER and DB_PASS in the connection URI string
+10. check URI string by using console.log()
+11. create async function run and call it by using run().catch(console.dir)
+12. add try and finally inside the run function.
+13. comment out await client.close() to keep the connection alive
 */
